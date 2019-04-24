17d1c99797ea17bb1afbb6b29fab3eb2dd7c7b0c
Tom Bonner <zerosavant@yahoo.com>
2019-04-24 15:48:12 -0500

    replaced with correct source document

--- fill.cpp
+++ fill.cpp
@@ -1,24 +1,37 @@
 // SortLargeFile.cpp
 #include <iostream>
+#include <iomanip>
 #include <iterator>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int f1(int value) {
-	return 2 * value;
-}
+template<typename T>
+void printArr(T &a, int size) {
+
+	for (int i = 0; i < size; i++) {
+		cout << fixed << setprecision(2) << a[i] << " ";
+	}
+};
 
 int main()
 {
-	int values[] = { 1,2,3,4,5 };
-	fill_n(values + 2, 2, 9);
-
 	ostream_iterator<int> output(cout, " ");
 
-	cout << "values: ";
-	copy(values, values + 5, output);
+	cout << fixed << setprecision(2);
 
-	cout << endl;
+	double array1[] = { 1.3, 2.4 ,4.5, 6.7, 9.0 };
+	cout << "contents of original array:\n\t";
+	printArr(array1, 5);
+
+	fill(array1, array1 + 3, 5.5);
+	cout << "\narray after fill():\n\t";
+	printArr(array1, 5);
+
+	fill_n(array1, 4, 6.9);
+	cout << "\narray after fill_n():\n\t";
+	printArr(array1, 5);
+
+	cout << endl << endl;
 	return 0;
 }
